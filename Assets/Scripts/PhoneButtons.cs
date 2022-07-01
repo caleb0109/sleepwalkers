56954be02e5ead44f1c74170d58596ef86b5e40b
Jess <45025366+ITZA-M3@users.noreply.github.com>
2022-07-01 00:05:48 -0700

    Phone Screen Switching
    Got the switching between screens to work, added OnClick listeners through the start and created two methods, one just for switching to home screen and the other to switch to a specific screen

--- Assets/Scripts/PhoneButtons.cs
+++ Assets/Scripts/PhoneButtons.cs
@@ -5,7 +5,30 @@ using UnityEngine.UI;
 
 public class PhoneButtons : MonoBehaviour
 {
-    public Canvas canSwitch;
-    
+    public GameObject nextScreen;
+    public Button bttn;
 
+    private void Start()
+    {
+        if (nextScreen == null)
+        {
+            bttn.onClick.AddListener(BackToHome);
+        }
+        else
+        {
+            bttn.onClick.AddListener(SwitchScreen);
+        }
+    }
+
+    // goes to the specific screen the button is tied to
+    public void SwitchScreen()
+    {
+        nextScreen.SetActive(true);
+    }
+
+    // sets itself inactive
+    public void BackToHome()
+    {
+        this.transform.parent.gameObject.SetActive(false); // set the parent screen to be inactive
+    }
 }
