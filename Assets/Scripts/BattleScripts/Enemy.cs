842c38cc5bbdf6d5482f5782d1ebb76cea770432
Jess <45025366+ITZA-M3@users.noreply.github.com>
2022-07-10 19:18:52 -0700

    Added animation for thugs getting hit

--- Assets/Scripts/BattleScripts/Enemy.cs
+++ Assets/Scripts/BattleScripts/Enemy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class Enemy : MonoBehaviour
@@ -8,11 +9,33 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     public float health;
     public float maxHealth;
+    public Text hitUI;
 
     public GameObject[] enemiesAttacks;
 
     public void TakeDamage(float damage)
     {
         health -= damage;
+        hitUI.text = damage.ToString();
+        hitUI.gameObject.GetComponent<Animator>().SetBool("gotHit", true);
+
+        StopAllCoroutines();
+        StartCoroutine(CountDown());
+    }
+
+    IEnumerator CountDown()
+    {
+        float duration = 2f;
+        while (duration > 0f)
+        {
+            duration -= Time.deltaTime;
+
+            if (duration < 1f)
+            {
+                hitUI.gameObject.GetComponent<Animator>().SetBool("gotHit", false);
+            }
+
+            yield return null;
+        }
     }
 }
