3fdefe8e36417cdfc871e32f83642562c5ebdd09
Jess <45025366+ITZA-M3@users.noreply.github.com>
2022-08-02 09:58:29 -0700

    Worked on fixing auto play and started entering animation
    Added couple of methods in the task manager as skeletons

---
+++ Assets/Scripts/TaskManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TaskManager : MonoBehaviour
+{
+    public List<TextAsset> tasksFiles;
+
+    private string currTask;
+    private List<string> taskList;
+    private List<string> requirements;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // updates the text on the to-do list widget and the app itself
+    private void UpdateTaskView()
+    {
+
+    }
+
+    // adds clues to the task (only in app view)
+    private void AddClue()
+    {
+
+    }
+
+    // moves to the next task in the list
+    private void CompleteTask()
+    {
+
+    }
+
+    private void CompleteSideTask()
+    {
+
+    }
+}
