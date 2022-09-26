b72b7ec92e0a0cd73ac2d3a9558a6386e45c8106
caleb0109 <cj6813@g.rit.edu>
2022-09-26 16:07:21 -0400

    Trying to add highlight on object, but no work

--- Assets/Scripts/Detect.cs
+++ Assets/Scripts/Detect.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Detect : MonoBehaviour
 {
     private GameObject detectedObj;
+    private SpriteRenderer sprite;
     private Color startColor;
 
     //Examine Window
@@ -22,13 +23,13 @@ public class Detect : MonoBehaviour
     {
 
         detectedObj = collision.gameObject;
-        startColor = detectedObj.color;
-        detectedObj.color = Color.yellow;
+        //startColor = detectedObj.color;
+        //detectedObj.color = Color.yellow;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        detectedObj.color = startColor;
+        //detectedObj.color = startColor;
         detectedObj = null;
     }
 
